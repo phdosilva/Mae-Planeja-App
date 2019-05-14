6ba037e9137fee5f7272bbdf31ef947e83495789
Otavio Vera Cruz Gomes <otavioveracruz@br-ufpe-11.local>
2019-05-14 12:07:13 -0300

    Adicionando Storage no PodFile.

---
+++ MaePlanejaCliente/MaePlanejaCliente/Models/Produto.swift
@@ -0,0 +1,22 @@
+//
+//  Produto.swift
+//  MaePlanejaCliente
+//
+//  Created by Otavio Vera Cruz Gomes on 14/05/19.
+//  Copyright © 2019 Cin Ufpe. All rights reserved.
+//
+
+import Foundation
+class Produto {
+    var nome_item: String?
+    var preco: String?
+    var imagem: String?
+    var mes: String?
+    
+    init(nome_item: String?, preco: String?, imagem: String?,mes:String?){
+        self.nome_item = nome_item
+        self.preco = preco
+        self.imagem = imagem
+        self.mes = mes
+    }
+}
--- MaePlanejaCliente/MaePlanejaCliente/ViewController.swift
+++ MaePlanejaCliente/MaePlanejaCliente/ViewController.swift
@@ -8,17 +8,36 @@
 
 import UIKit
 import FirebaseDatabase
+import FirebaseStorage
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        var ref: DatabaseReference!
-        ref = Database.database().reference()
-        ref.child("users").child("1").setValue(["username": "Name"])
-        ref.child("users").child("2").setValue(["username": "Allyson"])
-   
+    
+        //populate()
+    
+    }
+    
+    func populate(){
+        let ref: DatabaseReference! = Database.database().reference()
+        let produtoRef = ref.child("Produto")
+        let produtos = [Produto(nome_item: "Fralda", preco: "R$ 20,00", imagem: "", mes: "3"),
+                        Produto(nome_item: "Carrinho de bebê", preco: "R$ 350,00", imagem: "", mes: "6"),
+                        Produto(nome_item: "Mamadeira", preco: "R$ 15,00", imagem: "", mes: "5"),
+                        Produto(nome_item: "Berço", preco: "R$ 400,00", imagem: "", mes: "6"),
+                        Produto(nome_item: "Cômoda", preco: "R$ 300,00", imagem: "", mes: "6"),
+                        Produto(nome_item: "Macacão", preco: "R$ 20,00", imagem: "", mes: "5"),
+                        Produto(nome_item: "Fralda de pano", preco: "R$ 5,00", imagem: "", mes: "5"),
+                        Produto(nome_item: "Pano umidecido", preco: "R$ 10,00", imagem: "", mes: "5"),
+                        Produto(nome_item: "Banheira", preco: "R$ 350,00", imagem: "", mes: "5"),
+                        Produto(nome_item: "Pote de leite", preco: "R$ 5,00", imagem: "", mes: "7")]
+       
+        for produto in produtos{
+            let key = produtoRef.childByAutoId().key
+            let prod = ["nome_item":produto.nome_item,"preco":produto.preco,"imagem": produto.imagem,"mes":produto.mes]
+            produtoRef.child(key!).setValue(prod)
+        }
     }
 
 
