80ec028a704307d0ce16362158a3a8ec2ab7036f
Allyson Manoel Nascimento Venceslau <amnv@10-48-40-25.0101.ufpe.br.iosda.org>
2019-05-18 11:02:25 -0300

    Adicionando classes basicas

---
+++ MaePlanejaCliente/MaePlanejaCliente/Models/Endereco.swift
@@ -0,0 +1,9 @@
+//
+//  Endereco.swift
+//  MaePlanejaCliente
+//
+//  Created by Allyson Manoel Nascimento Venceslau on 18/05/19.
+//  Copyright © 2019 Cin Ufpe. All rights reserved.
+//
+
+import Foundation
---
+++ MaePlanejaCliente/MaePlanejaCliente/Models/Loja.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  MaePlanejaCliente
+//
+//  Created by Allyson Manoel Nascimento Venceslau on 16/05/19.
+//  Copyright © 2019 Cin Ufpe. All rights reserved.
+//
+
+import Foundation
+
+class {
