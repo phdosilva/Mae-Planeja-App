00dd2ea277bb58d11dac8f2001fbb52341b1b2c5
Jailson José da Silva junior <jjsj2@10-48-40-38.0101.ufpe.br.iosda.org>
2019-05-24 15:28:10 -0300

    implementação quase completa do slide de fotos

--- MaePlanejaCliente/MaePlanejaCliente/ProdutoDetalhesViewController.swift
+++ MaePlanejaCliente/MaePlanejaCliente/ProdutoDetalhesViewController.swift
@@ -8,25 +8,80 @@
 
 import UIKit
 
-class ProdutoDetalhesViewController: UIViewController {
+class ProdutoDetalhesViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
     var produto: Produto?
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var slides:[Slide] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        scrollView.delegate = self
+        
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubviewToFront(pageControl)
+        
+        //downloadImage(url: produto?.imagem ?? "", downloadImageView: self.imageView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createSlides() -> [Slide] {
+        
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        if let imgUrl = produto?.imagem{
+            downloadImage(url: imgUrl, downloadImageView: slide1.imageView)
+        }
+        
+        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        if let imgUrl = produto?.imagem{
+            downloadImage(url: imgUrl, downloadImageView: slide1.imageView)
+        }
+        
+        let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        if let imgUrl = produto?.imagem{
+            downloadImage(url: imgUrl, downloadImageView: slide1.imageView)
+        }
+        
+        return [slide1, slide2, slide3]
     }
-    */
-
+    
+    func setupSlideScrollView(slides : [Slide]) {
+        
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        //scrollView.isPagingEnabled = true
+        //scrollView.alwaysBounceVertical = false
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
+    
+    @objc func downloadImage(url:String,downloadImageView:UIImageView){
+        let imageURL = URL(string:url)!
+        
+        let dataTask = URLSession.shared.dataTask(with: imageURL) {
+            (data, reponse, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            if let data = data {
+                DispatchQueue.main.async {
+                    downloadImageView.image = UIImage(data: data)
+                }
+            }
+        }
+        
+        dataTask.resume()
+    }
 }
---
+++ MaePlanejaCliente/MaePlanejaCliente/Slide.swift
@@ -0,0 +1,22 @@
+//
+//  Slide.swift
+//  MaePlanejaCliente
+//
+//  Created by Jailson José da Silva junior  on 24/05/19.
+//  Copyright © 2019 Cin Ufpe. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
